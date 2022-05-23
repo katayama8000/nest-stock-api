6a933cfb1cb943db938013d515c4aed0ee8b3c47
katayama <tattu.0310@gmail.com>
2022-05-23 22:54:24 +0900

    ✨ post返す

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { get } from 'http';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/hello')
+  gethello() {
+    return 'aaa';
   }
+  // constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  @Post()
+  create(@Body() createUser: CreateUserDto) {
+    return createUser;
+  }
+}
