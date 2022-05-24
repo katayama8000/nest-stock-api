a5247c1c94e88f80a4e2dc22070c98dcc65b4acf
katayama <tattu.0310@gmail.com>
2022-05-24 12:51:47 +0900

    ✨ findAll

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
   @Post()
   create(@Body() createUser: CreateUserDto) {
-    return createUser;
+    return this.usersService.create(createUser);
   }
 }
